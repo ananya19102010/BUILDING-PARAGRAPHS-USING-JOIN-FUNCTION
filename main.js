e971ba78f74ba7c361ca411706ef1dabeeabb936
ananya19102010 <73178449+ananya19102010@users.noreply.github.com>
2021-03-29 20:32:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function getparagraph1(){
+    var inputs=[]
+    for (var i=1;i<=6;i++){
+        inputs.push(document.getElementById("div1_input_box_" + i).value);
+    }
+    inputs.join(". ");
+    document.getElementById("showparagraph1").innerHTML=inputs.join(". ");
+}   
+function getparagraph2(){
+    var inputs=[]
+    for (var i=1;i<=6;i++){
+        inputs.push(document.getElementById("div2_input_box_"+i).value);
+    }
+    inputs.join(". ");
+    document.getElementById("showparagraph2").innerHTML=inputs.join(". ");
+}
+    
